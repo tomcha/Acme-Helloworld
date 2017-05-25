238605639aa3e8455122be542a946893662a7eb2
tomcha <tomcha@tomcha.net>
2017-05-25 20:20:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Helloworld - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Helloworld;
+
+# DESCRIPTION
+
+Acme::Helloworld is ...
+
+# LICENSE
+
+Copyright (C) tomcha.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tomcha <tomcha@tomcha.net>
---
+++ lib/Acme/Helloworld.pm
@@ -0,0 +1,39 @@
+package Acme::Helloworld;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Helloworld - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Helloworld;
+
+=head1 DESCRIPTION
+
+Acme::Helloworld is ...
+
+=head1 LICENSE
+
+Copyright (C) tomcha.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tomcha E<lt>tomcha@tomcha.netE<gt>
+
+=cut
+
